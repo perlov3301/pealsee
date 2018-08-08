5f0a18e9807a89af9568e7de9d3a73215f3497ab
Alexander Perlov <perlov3301@gmail.com>
2018-08-08 20:11:24 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+   git config --global user.name "Your Name"
+   git config --global user.email you@example.com
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+<div>
+  <h1> hello World from a180807begn </h1>
+</div>
+  ,
+  document.getElementById('root'));
+registerServiceWorker();
